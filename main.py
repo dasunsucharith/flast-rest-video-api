bf6e10bb006855047a9bceec4f7ade12bd995ae6
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-07-02 22:15:28 +0530

    new

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from flask import Flask
 from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+names = {"dasun" : {"age" : 29, "gender" : "male"},
+         "rebecca" : {"age" : 26, "gender" : "female"}}
+
+class HelloWorld(Resource):
+    def get(self, name):
+        return names[name]
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
