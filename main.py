2bfe4150d98b19e4348e783026a65767e318abeb
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-07-03 08:12:18 +0530

    new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,12 +11,17 @@ video_put_args.add_argument("likes", type=int, help="Likes on the video")
 
 videos = {}
 
+def abort_if_video_id_not_exist(video_id):
+    if video_id not in videos:
+        abort()
 class Video(Resource):
     def get(self, video_id):
         return videos[video_id] 
 
     def put(self, video_id):
-        return {}
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -3,5 +3,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1" , {"likes": 10})
+response = requests.put(BASE + "video/1" , {"likes": 10, "name": "dasun", "views": 100000})
+print(response.json())
+input()
+response = requests.get(BASE + "video/1")
 print(response.json())
