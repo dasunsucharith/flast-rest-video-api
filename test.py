b0980ed8747db748753e5373662fa60d066c8fde
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-07-04 18:18:42 +0530

    new

--- test.py
+++ test.py
@@ -3,8 +3,19 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1" , {"likes": 10, "name": "dasun", "views": 100000})
-print(response.json())
+data = [{"likes": 10, "name": "HTML", "views": 100000},
+        {"likes": 100, "name": "CSS", "views": 1000000},
+        {"likes": 1000, "name": "Javascript", "views": 10000000},
+        {"likes": 10000, "name": "Python", "views": 100000000}]
+
+for i in range(len(data)):
+    response = requests.put(
+        BASE + "video/" + str(i), data[i])
+    print(response.json())
+
 input()
-response = requests.get(BASE + "video/1")
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + "video/6")
 print(response.json())
