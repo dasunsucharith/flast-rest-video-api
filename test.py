c29c4bc249deb1c7ce166f78b0f04e80a395b1a7
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-07-05 12:05:53 +0530

    new

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ import requests
 BASE = "http://127.0.0.1:5000/"
 
 
-response = requests.patch(f"{BASE}video/2", {"views": "99"})
+response = requests.patch(f"{BASE}video/2", {"views": "99", "likes": "101"})
 print(response.json())
